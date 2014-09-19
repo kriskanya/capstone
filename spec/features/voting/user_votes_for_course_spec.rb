fbf71efaa40eb8cfca68a52c44ba86476a7fc10e
Kristian Kanya <kriskanya@gmail.com>
2014-09-19 14:11:00 -0500

    added downvote to user_votes_for_course_spec

--- spec/features/voting/user_votes_for_course_spec.rb
+++ spec/features/voting/user_votes_for_course_spec.rb
@@ -2,14 +2,14 @@
 # I want to click on the upvote arrow on a course
 # And ensure that the course has been upvoted by me
 
-feature "User upvotes a course" do
+feature "User votes on a course" do
   background do
     @user = Fabricate(:user)
     @course = Fabricate(:course)
     login_as @user
   end
 
-  scenario "Happy Path" do
+  scenario "Happy Path - Upvote" do
     visit '/'
     find('.up-arrow-link').click
     @course.get_likes.size.should eq 1
@@ -17,5 +17,14 @@ feature "User upvotes a course" do
     (@user.voted_up_on? @course).should eq true
     within('.course_likes') { expect(page).to have_content(1) }
     # arrow button should turn green
+  end
+
+  scenario "Happy Path - Downvote" do
+    visit '/'
+    find('.down-arrow-link').click
+    @course.get_dislikes.size.should eq 1
+    (@user.voted_for? @course).should eq true
+    (@user.voted_down_on? @course).should eq true
+    within('.course_likes') { expect(page).to have_content(0) }
   end
 end
