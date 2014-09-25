d6321dd33ac215f223dedd96c6909169d5c97173
Kristian Kanya <kriskanya@gmail.com>
2014-09-25 17:17:13 -0500

    changed comment_text from string to text, added validation so that only logged in users can comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
   acts_as_votable
 
   validates_presence_of :comment_text
+  validates_presence_of :user_id
 
   belongs_to :user
   belongs_to :course
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925162153) do
+ActiveRecord::Schema.define(version: 20140925215604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: true do |t|
-    t.string   "comment_text"
+    t.text     "comment_text"
     t.integer  "user_id"
     t.integer  "course_id"
     t.datetime "created_at"
